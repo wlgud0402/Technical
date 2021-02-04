554829d4c459dc9c720914224e0eed6a960534f2
wlgud0402 <wlgudrlgus@naver.com>
2021-02-05 00:10:25 +0900

    doing test

--- testing/products/tests/test_models.py
+++ testing/products/tests/test_models.py
@@ -2,13 +2,16 @@ from mixer.backend.django import mixer
 import pytest
 
 
-@pytest.mark.django_db
-class TestModels:
+@pytest.fixture
+def product(request, db):
+    return mixer.blend('products.Product', qunatity=request.param)
 
-    def test_product_is_in_stock(self):
-        product = mixer.blend('products.Product', quantity=1)
-        assert product.is_in_stock == True
 
-    def test_product_is_not_in_stock(self):
-        product = mixer.blend('products.Product', quantity=0)
-        assert product.is_in_stock == False
+@pytest.mark.parametrize('product', [1], indirect=True)
+def test_product_is_in_stock(product):
+    assert product.is_in_stock == True
+
+
+@pytest.mark.parametrize('product', [0], indirect=True)
+def test_product_is_not_in_stock(product):
+    assert product.is_in_stock == False
--- testing/products/tests/test_views.py
+++ testing/products/tests/test_views.py
@@ -3,26 +3,34 @@ from django.urls import reverse
 from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
+from django.test import TestCase
 import pytest
 
 
-@pytest.mark.django_db
-class TestViews:
+@pytest.fixture(scope='module')
+def factory():
+    print("FACTORY INSTANCIATED")
+    return RequestFactory()
 
-    def test_product_detail_authenticated(self):
-        mixer.blend('products.Product')
-        path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
-        request.user = mixer.blend(User)
 
-        response = product_detail(request, pk=1)
-        assert response.status_code == 200
+@pytest.fixture
+def product(db):
+    return mixer.blend('products.Product')
 
-    def test_product_detail_unauthenticated(self):
-        mixer.blend('products.Product')
-        path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
-        request.user = AnonymousUser()
 
-        response = product_detail(request, pk=1)
-        assert 'accounts/login' in response.url
+def test_product_detail_authenticated(factory, product):
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = mixer.blend(User)
+
+    response = product_detail(request, pk=1)
+    assert response.status_code == 200
+
+
+def test_product_detail_unauthenticated(factory, product):
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = AnonymousUser()
+
+    response = product_detail(request, pk=1)
+    assert 'accounts/login' in response.url
