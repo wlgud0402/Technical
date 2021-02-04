1c752976cae0659518f31a8d51ad6a26863eb03b
wlgud0402 <wlgudrlgus@naver.com>
2021-02-04 22:44:03 +0900

    start testing

---
+++ testing/products/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+# Create your views here.
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
